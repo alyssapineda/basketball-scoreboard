1ccec610ad41e82a3e3dbdd9b320af8620f17d46
alyssa <110448688+alyssapineda@users.noreply.github.com>
2022-10-19 22:22:10 +1300

    final tentative

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const buttonAudio = new Audio("/audio/click.wav");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    buttonAudio.play();
+  });
+});
+
 let homeScore = document.querySelector("#score-home");
 let guestScore = document.querySelector("#score-guest");
 let homeFoul = document.querySelector("#foul-el-home");
@@ -6,8 +15,9 @@ let hometimeOut = document.querySelector("#timeout-el-home");
 let guesttimeOut = document.querySelector("#timeout-el-guest");
 
 let period = document.querySelector("#period-el");
-let timer = document.querySelector("#timer-el");
+let timer_countdown = document.querySelector("#timer-el");
 
+//INITIALISING COUNTERS
 home_counter = 0;
 guest_counter = 0;
 foul_home_counter = 0;
@@ -82,10 +92,63 @@ function addPeriod() {
     period.textContent = period_counter;
 }
 
-//START TIMER
+//SETTING UP TIMER
+let startingTime = 10;
+let time = startingTime * 60; //in seconds
+let timerPaused = true;
 
-//PAUSE TIMER
+//TIMER DISPLAY FUNCTION
+function displayTime(time) {
+    const minute = Math.floor(time/60)
+    const seconds = Math.floor(time % 60);
+    timer_countdown.textContent = `${minute<10 ? '0': ''}${minute}:${seconds<10 ? '0': ''}${seconds}`
+}
 
-// RESET TIMER
+const countDown = setInterval(() => {
+    if (!timerPaused) {
+        time--;
+        displayTime(time)
 
+        if(time <= 0 || time <1) {
+            clearInterval(countDown);
+        }
+    }
+},1000)
+
+//START TIMER FUNCTION
+function timerStart() {
+    timerPaused = false;
+}
+//PAUSE TIMER FUNCTION
+function timerPause() {
+    timerPaused = true;
+}
+
+// RESET TIMER FUNCTION
+function timerReset() {
+    timer_countdown.textContent = '10:00';
+    time = startingTime * 60;
+    timerPaused = true;
+}
 // NEW GAME
+function newGame() {
+    home_counter = 0;
+    guest_counter = 0;
+    foul_home_counter = 0;
+    foul_guest_counter = 0;
+    timeOut_home_counter = 0;
+    timeOut_guest_counter = 0;
+    period_counter = 0;
+
+    homeScore.textContent = home_counter;
+    guestScore.textContent = guest_counter;
+    homeFoul.textContent = foul_home_counter;
+    guestFoul.textContent = foul_guest_counter;
+    hometimeOut.textContent = timeOut_home_counter;
+    guesttimeOut.textContent = timeOut_guest_counter;
+    period.textContent = period_counter;
+
+    timer_countdown.textContent = '10:00';
+    time = startingTime * 60;
+    timerPaused = true;
+}
